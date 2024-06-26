197412eca8517bee9c79ee755df8a939a7867c6e
ngocDa <ngocda@ows.com.vn>
2024-06-26 12:50:56 +0700

    commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Text,
   TextInput,
@@ -6,18 +6,61 @@ import {
   StyleSheet,
   View,
   ImageBackground,
+  Alert,
 } from 'react-native';
 import {images} from './src/assets/images/images';
+import {createClient} from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
 const InputData = () => {
   const [temperature, setTemperature] = useState('');
   const [displayText, setDisplayText] = useState('');
 
+  const supabaseUrl = 'https://atamzgfzgyynoqqdnbup.supabase.co';
+  const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0YW16Z2Z6Z3l5bm9xcWRuYnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyOTg0NDEsImV4cCI6MjAzNDg3NDQ0MX0.Ner2Wvuop0mILVgNkhI_Q0_XNgzC32pKRTkAhQlWA2I';
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      detectSessionInUrl: false,
+    },
+  });
+
+  const fetchData = async () => {
+    const {data, error} = await supabase
+      .schema('public')
+      .from('ocd')
+      .select('*')
+      .order('id', {ascending: false})
+      .limit(1);
+
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Data', data);
+      setDisplayText(`${data[0].value}cd`);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const changeTemperature = async (temperature: number) => {
+    const {data, error} = await supabase
+      .from('ocd')
+      .insert([{value: temperature}])
+      .select();
+
+    if (!!data) {
+      console.log('dataCHange', data);
+      Alert.alert('success');
+    }
+  };
   const handleTemperatureInput = (text: React.SetStateAction<string>) => {
     setTemperature(text);
   };
   const handleSubmit = () => {
     setDisplayText(`${temperature} cd`);
+    changeTemperature(Number(temperature));
   };
 
   return (
